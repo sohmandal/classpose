b0d52394f19b4857ff00aba04867664a38031d2b
josegcpa <jose.almeida@research.fchampalimaud.org>
2026-05-13 17:58:23 +0100

    working on improving memory use for dataset to hdf5 conversion

--- paper_experiments/dataset_to_hdf5.py
+++ paper_experiments/dataset_to_hdf5.py
@@ -7,7 +7,6 @@ from pathlib import Path
 
 from classpose.train_utils import _process_train_test
 from classpose.log import get_logger
-from classpose.train_utils import get_class_counts, get_instance_counts
 
 logger = get_logger(__name__)
 
@@ -39,11 +38,58 @@ def parse_args():
     return parser.parse_args()
 
 
-def resize_and_increment(dataset, key, new_array):
-    logger.info("Appending array with shape %s to %s", new_array.shape, key)
-    new_shape = new_array.shape
-    dataset[key].resize((dataset[key].shape[0] + new_shape[0], *new_shape[1:]))
-    dataset[key][-new_shape[0] :] = new_array
+def resize_and_increment(dataset, key, new_items, n: int):
+    item_shape = dataset[key].shape[1:]
+    logger.info("Appending %d items with shape %s to %s", n, item_shape, key)
+    old_size = dataset[key].shape[0]
+    dataset[key].resize((old_size + n, *item_shape))
+    for i, item in enumerate(new_items):
+        dataset[key][old_size + i] = item
+
+
+def get_class_counts(Y: list[np.ndarray], n_classes: int) -> np.ndarray:
+    """
+    Get class counts directly from class array.
+
+    Args:
+        Y (np.ndarray): Array of labels (1 channel: class).
+        n_classes (int): Number of classes.
+
+    Returns:
+        np.ndarray: Array of class counts.
+    """
+    return np.bincount(
+        np.concatenate([y.ravel() for y in Y]), minlength=n_classes
+    )
+
+
+def get_instance_counts(
+    processed_labels: np.ndarray | list[np.ndarray],
+    classes: np.ndarray | list[np.ndarray],
+    n_classes: int | None = None,
+) -> np.ndarray:
+    """
+    Simplified version of get instance counts.
+
+    Args:
+        processed_labels (np.ndarray): instance + flows array or list of such
+            arrays.
+        classes (np.ndarray): class array or list of class arrays.
+        n_classes (int | None, optional): Number of classes. If None, it will be
+            inferred from the labels. Defaults to None.
+
+    Returns:
+        np.ndarray: Array of instance counts.
+    """
+    if n_classes is None:
+        n_classes = np.max([im.max() for im in classes]) + 1
+    counts = np.zeros((len(processed_labels), n_classes))
+    for i in range(len(processed_labels)):
+        ins = processed_labels[i][0]
+        cla = classes[i]
+        for j in range(n_classes):
+            counts[i, j] = np.unique(ins[cla == j]).size
+    return counts
 
 
 def load_data(data_dir):
@@ -67,6 +113,9 @@ def load_data(data_dir):
     transp = (2, 0, 1)
     train_images = [np.transpose(im, transp) for im in images]
     train_labels = [np.transpose(im, transp) for im in labels]
+
+    del images
+    del labels
 
     return train_images, train_labels
 
@@ -100,9 +149,10 @@ def main():
             shape=(0, args.n_classes),
             maxshape=(None, args.n_classes),
         )
-        for data_dir in args.data_dirs:
+        for i, data_dir in enumerate(args.data_dirs):
             logger.info("Processing %s", data_dir)
-            train_images, train_labels = load_data(data_dir)
+            if i > 0:
+                train_images, train_labels = load_data(data_dir)
             logger.info("Processing data (computing flows, filtering)...")
             out = _process_train_test(
                 train_data=train_images,
@@ -116,27 +166,30 @@ def main():
             processed_labels = out[1]
             processed_classes = out[5]
 
-            final_labels = [
+            # instantiate as an iterator to avoid loading all labels in memory
+            final_labels = (
                 np.concatenate((y[:1], y_class, y[1:]), axis=0)
-                for y, y_class in tqdm(
-                    zip(processed_labels, processed_classes),
-                    total=len(processed_labels),
-                )
-            ]
+                for y, y_class in zip(processed_labels, processed_classes)
+            )
 
+            logger.info("Computing class counts")
             class_count_accumulator += get_class_counts(
-                train_labels, args.n_classes
+                processed_classes, args.n_classes
             )
 
-            image_data = np.stack(processed_images)
-            label_data = np.stack(final_labels)
+            logger.info("Getting instance counts")
             instance_counts = get_instance_counts(
-                train_labels, n_classes=args.n_classes
+                processed_labels, processed_classes, n_classes=args.n_classes
             )
 
-            resize_and_increment(f, "images", image_data)
-            resize_and_increment(f, "labels", label_data)
-            resize_and_increment(f, "instance_counts", instance_counts)
+            logger.info("Writing data")
+            n = len(processed_images)
+            resize_and_increment(f, "images", processed_images, n=n)
+            resize_and_increment(f, "labels", final_labels, n=n)
+            resize_and_increment(f, "instance_counts", instance_counts, n=n)
+
+            del processed_images
+            del instance_counts
 
         logger.info("Storing class counts")
         f["class_counts"] = class_count_accumulator
